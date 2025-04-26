2291ecda69139d3259b0307e7de066bcd9164f0e
Tim Russell <timrussell@outlook.com>
2025-04-25 22:47:45 -0400

    Initial Commit

---
+++ Mariner Studio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mariner Studio
+//
+//  Created by Timothy Russell on 4/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mariner Studio/Mariner_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mariner_StudioApp.swift
+//  Mariner Studio
+//
+//  Created by Timothy Russell on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Mariner_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
