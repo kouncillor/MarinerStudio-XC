82832f211f69a1a4816774394b802bc25a90e80c
Tim Russell <timrussell@outlook.com>
2025-05-16 20:16:44 -0400

    Fighting with Claude

--- Mariner Studio/Tandm/MarinerMapData.swift
+++ Mariner Studio/Tandm/MarinerMapData.swift
@@ -1,3 +1,58 @@
+//
+//import MapKit
+//import CoreLocation
+//
+//struct MarinerMapData: Decodable {
+//    let navobjects: [NavObject]
+//    let centerLatitude: CLLocationDegrees
+//    let centerLongitude: CLLocationDegrees
+//    let latitudeDelta: CLLocationDegrees
+//    let longitudeDelta: CLLocationDegrees
+//    
+//    var region: MKCoordinateRegion {
+//        let center = CLLocationCoordinate2D(latitude: centerLatitude, longitude: centerLongitude)
+//        let span = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+//        return MKCoordinateRegion(center: center, span: span)
+//    }
+//}
+//
+//
+//class NavObject: NSObject, Decodable, MKAnnotation {
+//    enum NavObjectType: Int, Decodable {
+//        case navunit
+//        case tidalheightstation
+//        case tidalcurrentstation
+//    }
+//    
+//    var type: NavObjectType = .navunit
+//    var name: String = "" // Added name property
+//    var objectId: String = "" // Added objectId to store the original identifier
+//    var currentBin: Int? = nil // Added currentBin for tidal current stations
+//    
+//    private var latitude: CLLocationDegrees = 0
+//    private var longitude: CLLocationDegrees = 0
+//    
+//    @objc dynamic var coordinate: CLLocationCoordinate2D {
+//        get {
+//            return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//        }
+//        set {
+//            latitude = newValue.latitude
+//            longitude = newValue.longitude
+//        }
+//    }
+//    
+//    // MARK: - Title for annotation callout
+//    @objc var title: String? {
+//        return name
+//    }
+//}
+
+
+
+
+
+
 
 import MapKit
 import CoreLocation
@@ -22,6 +77,7 @@ class NavObject: NSObject, Decodable, MKAnnotation {
         case navunit
         case tidalheightstation
         case tidalcurrentstation
+        case buoystation // Added new case for buoy stations
     }
     
     var type: NavObjectType = .navunit
