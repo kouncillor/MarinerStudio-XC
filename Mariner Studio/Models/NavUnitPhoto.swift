9899570518bac68c7dc9ec4a9213b3d3258872a5
Tim Russell <timrussell@outlook.com>
2025-06-01 03:14:08 -0400

    another sigh

--- Mariner Studio/Models/NavUnitPhoto.swift
+++ Mariner Studio/Models/NavUnitPhoto.swift
@@ -1,36 +1,3 @@
-//import Foundation
-//
-//// Model for NavUnitPhoto (converted from C#)
-//struct NavUnitPhoto: Identifiable {
-//    let id: Int
-//    let navUnitId: String
-//    let filePath: String
-//    let fileName: String
-//    let thumbPath: String?
-//    let description: String?
-//    let createdAt: Date
-//    
-//    // Initializer with default values
-//    init(
-//        id: Int = 0,
-//        navUnitId: String,
-//        filePath: String,
-//        fileName: String,
-//        thumbPath: String? = nil,
-//        description: String? = nil,
-//        createdAt: Date = Date()
-//    ) {
-//        self.id = id
-//        self.navUnitId = navUnitId
-//        self.filePath = filePath
-//        self.fileName = fileName
-//        self.thumbPath = thumbPath
-//        self.description = description
-//        self.createdAt = createdAt
-//    }
-//}
-
-
 import Foundation
 
 // Model for NavUnitPhoto (converted from C#)
@@ -42,7 +9,6 @@ struct NavUnitPhoto: Identifiable {
     let thumbPath: String?
     let description: String?
     let createdAt: Date
-    let cloudKitRecordID: String? // NEW: Track CloudKit record ID
     
     // Initializer with default values
     init(
@@ -52,8 +18,7 @@ struct NavUnitPhoto: Identifiable {
         fileName: String,
         thumbPath: String? = nil,
         description: String? = nil,
-        createdAt: Date = Date(),
-        cloudKitRecordID: String? = nil // NEW: CloudKit record ID
+        createdAt: Date = Date()
     ) {
         self.id = id
         self.navUnitId = navUnitId
@@ -62,6 +27,6 @@ struct NavUnitPhoto: Identifiable {
         self.thumbPath = thumbPath
         self.description = description
         self.createdAt = createdAt
-        self.cloudKitRecordID = cloudKitRecordID // NEW
     }
 }
+
